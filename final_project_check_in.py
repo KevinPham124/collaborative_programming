c4758bfe91dc987b023ccc2a4c40b0e2df43b7d3
Kirk Laryea <klaryea@terpmail.umd.edu>
2024-04-12 05:44:12 -0400

    Added Class and load data function

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -1,4 +1,20 @@
 # INST326 Final project check-in
+import argparse
 
+#Kirk Laryea
+class FinanceManager:
+    def __init(self,filepath):
+        self.filepath=filepath
+        self.data=[]
+    def load_data(self):
+        try:
+            with open(self.filepath,'r',encoding='utf-8') as f:
+                self.data=[]
+                for line in f.readlines():
+                    content=line.strip().splt(',')
+                    self.data.append(content)
+        except FileNotFoundError:
+            print("This file does not exist, do you mean 'finance.txt?'")
+                
 #Kevin Pham
 def add_budget():
