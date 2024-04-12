3ac4c7d01b346967b5763e35b6c78763be5cd46d
Kevin <kpham124@terpmail.umd.edu>
2024-04-12 19:34:40 -0400

    Kevin's commit

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -1,9 +1,15 @@
-# INST326 Final project check-in
+# INST326 Final project check-in (Group 6)
+
+# Currently, everyone has written their own functions and classes that will 
+# address certain challenges for the final project. Our next step is to analyze
+# each member's code and find out how to make them function better with each 
+# other and become one cohesive program.
+
 import argparse
 
 #Kirk Laryea
 class FinanceManager:
-    def __init(self,filepath):
+    def __init__(self,filepath):
         self.filepath=filepath
         self.data=[]
     def load_data(self):
@@ -11,16 +17,30 @@ class FinanceManager:
             with open(self.filepath,'r',encoding='utf-8') as f:
                 self.data=[]
                 for line in f.readlines():
-                    content=line.strip().splt(',')
+                    content=line.strip().split(',')
                     self.data.append(content)
         except FileNotFoundError:
             print("This file does not exist, do you mean 'finance.txt?'")
                 
 #Kevin Pham
-def add_budget():
+class Budget:
+    def __init__(self):
+        self.expenses = {}
+        
+    def set_budget(self, expense_category, amount):
+        self.expenses[expense_category] = amount
+        
+    def get_budget(self, category):
+        return self.expenses.get(category, 0)
+ 
+    def delete_budget(self, category):
+        if category in self.expenses:
+            del self.expenses[category]
     
     
 #Miles Rousseau
+import datetime
+
 def get_transactions(filter_type=None, filter_value=None):
     data = load_data
     for entry in data:
