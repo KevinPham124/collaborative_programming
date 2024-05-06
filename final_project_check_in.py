ef6f11326ec39d0307368a5b810af6774e4d43ba
Milesbam <milesr@terpmail.umd.edu>
2024-05-06 17:03:27 -0400

    Reworked/Updated code to work correctly with the financemanager class.

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -7,6 +7,7 @@
 
 import argparse
 import sys
+from datetime import datetime
 
 #Kirk Laryea      
 class FinanceManager:
@@ -54,31 +55,45 @@ def budget_per_month(month,year,income, expenses):
     return budget
     
 #Miles Rousseau
-from datetime import datetime
+
 
 class findtransactions:
-    # Kinda of works with hard-codes values (FOR NOW).
-    # Goal is to do something like (python 'script-here' date 05-0x3-2024)
+    """
+    
+    Place Holder
+    
+    """
 
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        
 
     def get_transactions(self, filter_type=None, filter_value=None):
         
-        for entry in self.statement:
-         if entry['date']:
-            entry['date'] = datetime.strptime(entry['date'])
-            
+        data = FinanceManager(self.path).load_data()
         filtered_data = []
+        for entry in data:
+            if entry['date']:
+                entry['date'] = datetime.strptime(entry['date'], '%m%Y').date()
+            entry['Income'] = float(entry['Income'].replace('$', '').replace(',', ''))
+            entry['Total Expenses'] = float(entry['Total Expenses'].replace('$', '').replace(',', ''))
+            entry['Savings'] = float(entry['Savings'].replace('$', '').replace(',', ''))
+
+
+                
         if filter_type and filter_value:
-            for entry in self.statement:
-                if filter_type == 'date' and entry['date'] == datetime.strptime(entry['date']):
+            for entry in data:
+                if filter_type == 'date' and entry['date'] == datetime.strptime(filter_value, '%m%Y').date():
                     filtered_data.append(entry)
-                elif filter_type == 'amount' and float(entry['amount']) == float (filter_value):
+                elif filter_type == 'income' and float(entry['Income']) == float (filter_value):
                     filtered_data.append(entry)
-                elif filter_type == 'type' and entry['type'] == filter_value:
+                elif filter_type == 'savings' and entry['Savings'] == filter_value:
+                    filtered_data.append(entry)
+                elif filter_type == 'total_expenses' and entry ['Total Expenses'] == float(filter_value):
                     filtered_data.append(entry)
         else:
-            filtered_data = self.statement
-            
+            filtered_data = self.statement   
         return filtered_data
     
     
