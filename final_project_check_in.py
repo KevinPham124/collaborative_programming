ce39a9707eebb9339a7edbedeab507555ad4e82c
kirklaryea <90470238+kirklaryea@users.noreply.github.com>
2024-05-08 19:17:57 -0400

    changes

--- final_project_check_in.py
+++ final_project_check_in.py
@@ -13,12 +13,12 @@ import matplotlib.pyplot as plt
 
 #Kirk Laryea      
 class Expense:
-    def init(self, name, category, amount):
+    def __init__(self, name, category, amount):
         self.name = name
         self.category = category
         self.amount = amount
 
-    def repr(self):
+    def __repr__(self):
         return f"Expense: '{self.name}' - [{self.category}, ${self.amount:.2f}]"        
                 
 #Kevin Pham
